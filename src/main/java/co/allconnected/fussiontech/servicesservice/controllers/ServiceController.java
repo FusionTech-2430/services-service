30bbd0005fd3fbc113defaf2cf8ee1b497d1966d
DiegoPardoMontero <di-pardo@javeriana.edu.co>
2024-09-23 12:55:21 -0500

    :sparkles: Add basic functionality

---
+++ src/main/java/co/allconnected/fussiontech/servicesservice/controllers/ServiceController.java
@@ -0,0 +1,9 @@
+package co.allconnected.fussiontech.servicesservice.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/services")
+public class ServiceController {
+}
